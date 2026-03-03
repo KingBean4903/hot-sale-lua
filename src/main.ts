ce73f805270715f971ea79c54a18254550dcdcb3
KingBean4903 <davidkanyango065@gmail.com>
2026-03-03 16:02:41 +0300

    Added idempotency

--- src/main.ts
+++ src/main.ts
@@ -19,25 +19,105 @@ const RELEASE_STOCK_LUA = fs.readFileSync(
 `./src/redis/scripts/releaseStock.lua`,
 								'utf8'
 );
-
+const RESERVE_STOCK_LUA = fs.readFileSync(
+`./src/redis/scripts/reserveStock.lua`,
+								'utf8'
+);
 fastify.post('/init-stock', async (request, reply) => {
 								
 								await redis.hset('flashsale:fs_123:stock',
-																								'available', 3,
+																								'available', 50,
 																								'reserved', 0,
 																								'sold', 0); 
 });
 
 fastify.post('/reserve', async (request, reply) => {
 
-								await addReservation({
+								/*const res = await reserveLua({
 																saleId: 'fs_123',
 																reservationId: 'res_8799',
 																ttlMs: 10_000
-								});
+								});*/
+
+								const attempts = 100;
+
+								const results = await Promise.all(
+																Array.from({ length: attempts }, ( _, k) => 
+																								reserveLua({ 
+																																saleId: 'fs_123', 
+																																reservationId: 'res_772A_1', 
+																																ttlMs: 60_000,
+																																custId: 'cust_343'
+																								})
+																))
+
+								console.log(`Results ${results}`)
+
+								const success = results.filter(r => r === 3).length;
+								const failed = results.filter(r => r === 0).length;
+
+								console.log('Success: ', success)
+								console.log('Failed: ', failed)
+
+								const stock  = await redis.hgetall(`flashsale:fs_123:stock`);
+								console.log('Final stock', stock);
+
+								return { success, failed, stock }
 })
 
 const releaseStockSha = await redis.script("LOAD", RELEASE_STOCK_LUA);
+const releaseStockLuaSha = await redis.script("LOAD", RESERVE_STOCK_LUA);
+
+const reserveLua = async({ saleId, reservationId, ttlMs, custId }) => {
+
+								const expiresAt = Date.now() + ttlMs;
+								const numkeys = 3;
+								const quantity = '1';
+								const MAX_PER_CUSTOMER = '2';
+								
+								try {
+																const res = await redis.evalsha(
+																								releaseStockLuaSha,
+																								numkeys,
+																								[`flashsale:${saleId}:stock`,
+																								 `flashsale:${reservationId}:reservations`,
+																								 `flashsale:${saleId}:cust:${custId}`
+																								],
+																								[
+																								   quantity, 
+																											reservationId, 
+																											expiresAt,
+																											MAX_PER_CUSTOMER
+																]);
+
+																																console.log(`Reserve res ${res}`)
+								} catch(err) {
+
+																if (err.message.includes('NOSCRIPT')) {
+																								const releaseStockLuaSha = 
+																																await redis.script("LOAD", RESERVE_STOCK_LUA);
+
+																const res = await redis.evalsha(
+																								releaseStockLuaSha,
+																								numkeys,
+																								[`flashsale:${saleId}:stock`,
+																								 `flashsale:${reservationId}:reservations`,
+																								 `flashsale:${saleId}:cust:${custId}`
+																								],
+																								[
+																								   quantity, 
+																											reservationId, 
+																											expiresAt,
+																											MAX_PER_CUSTOMER
+																]);
+																} else {
+																								console.log(`Reserve log erri ${err}`)
+																								throw err;
+																}
+
+								}
+
+}
 
 const addReservation = async({ saleId, reservationId, ttlMs }) => {
 								const expiresAt = Date.now() + ttlMs;
@@ -61,9 +141,9 @@ const reapExpired = async (saleId) => {
 								)
 
 								for (const reservationId of expired) {
-																await evalReleaseStock(`${saleId}`)
+																await evalReleaseStock(`flashsale:${saleId}:stock`)
 																await redis.zrem(
-																								`flashsale:${saleId}:reservations`,
+																								`flashsale:${reservatioId}:reservations`,
 																								reservationId
 																)
 								}
@@ -75,7 +155,8 @@ const evalReleaseStock = async (keys) => {
 																await redis.evalsha(releaseStockSha, 1, keys, 1 );
 								} catch(err) {
 																if (err.message.includes('NOSCRIPT')) {
-																								const releaseStockSha = await redis.script("LOAD", RELEASE_STOCK_LUA)
+																								const releaseStockSha = 
+																																await redis.script("LOAD", RELEASE_STOCK_LUA)
 																								await redis.evalsha(releaseStockSha, 1, keys, 1 );
 																} else {
 																								throw err
@@ -88,12 +169,12 @@ const evalReleaseStock = async (keys) => {
 								for(const saleId of activeSales) {
 																await reapExpired(saleId);
 								}
-}, REAP_INTERVAL_MS)
-*/
+}, REAP_INTERVAL_MS) */
+
 
 const start = async () => {
 								try {
-																await fastify.listen({ port: 6500 })
+																await fastify.listen({ port: 8500 })
 								} catch (err) {
 																fastify.log.error(err)
 																process.exit(1)
--- src/redis/scripts/reserveStock.lua
+++ src/redis/scripts/reserveStock.lua
@@ -4,14 +4,28 @@
 -- ARGV[2] = reservationId
 -- ARGV[3] = expiresAt
 
+
+
+local added = redis.call("ZADD", KEYS[2], "NX", ARGV[3], ARGV[2])
+
+if added == 0 then
+								return 3
+end
+
 local available = tonumber(redis.call("HGET", KEYS[1], "available"))
 
 if available < tonumber(ARGV[1]) then
   return 0
 end
 
+local customerReserved = tonumber(redis.call("HGET", KEYS[3]) or "0")
+if (customerReserved + tonumber(ARGV[1])) > tonumber(ARGV[4]) then
+								redis.call("ZREM", KEYS[2], ARGV[2])
+								return 2
+end
+
 redis.call("HINCRBY", KEYS[1], "available", -ARGV[1])
 redis.call("HINCRBY", KEYS[1], "reserved", ARGV[1])
-redis.call("ZADD", KEYS[2], ARGV[3], ARGV[2])
+redis.call("INCRBY", KEYS[3], ARGV[1])
 
 return 1
